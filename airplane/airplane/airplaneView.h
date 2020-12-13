53973c845816e1ec2361c2d0bde3d6491f4d81fc
유한 이 <l0u0h0@naver.com>
2020-12-13 20:35:37 +0900

    2020_12_13

---
+++ airplane/airplane/airplaneView.h
@@ -0,0 +1,69 @@
+﻿
+// airplaneView.h: CairplaneView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CairplaneView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CairplaneView() noexcept;
+	DECLARE_DYNCREATE(CairplaneView)
+
+// 특성입니다.
+public:
+	CairplaneDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CairplaneView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	int back_x;
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	int type;
+	afx_msg void OnChar(UINT nChar, UINT nRepCnt, UINT nFlags);
+	int index;
+	int plane_y;
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	int WinBottom;
+	int WinRight;
+	int map_idx;
+	int text_y;
+	int bird_index;
+	int bird_x;
+	int bird_y;
+	int crashTest(int bird_x, int bird_y, int plane_y);
+	void Restart();
+	int bird_fly;
+	int bird_type;
+};
+
+#ifndef _DEBUG  // airplaneView.cpp의 디버그 버전
+inline CairplaneDoc* CairplaneView::GetDocument() const
+   { return reinterpret_cast<CairplaneDoc*>(m_pDocument); }
+#endif
+
---
+++ airplane/airplane/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// airplane.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_airplaneTYPE                130
+#define IDB_BIT_BACK3                   310
+#define IDB_BIT_BACK4                   311
+#define IDB_BIT_BACK6                   312
+#define IDB_BIT_BACK2                   313
+#define IDB_BIT_BACK1                   314
+#define IDB_BIT_BACK7                   315
+#define IDB_BIT_BACK5                   316
+#define IDB_BIT_BACK8                   317
+#define IDB_PNG1                        319
+#define IDB_BITMAP1                     320
+#define IDB_BIT_END                     320
+#define IDB_BITMAP2                     322
+#define IDB_BIT_AIRPLANE                322
+#define IDB_BITMAP3                     323
+#define IDB_BIT_GAMEOVER                323
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        324
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
